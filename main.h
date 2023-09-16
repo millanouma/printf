214ed3d9ac95d5c0e0ae803e6b13af71472bc1ac
Nj3r1 <susannganga884@gmail.com>
2023-09-16 21:38:04 +0300

    update

--- main.h
+++ main.h
@@ -8,6 +8,9 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-
-
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
 #endif
