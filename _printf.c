c6cd5f36fc2d7569c5a4df550ae4a9e1bb55a563
millanouma <millanouma@gmail.com>
2023-09-18 12:07:54 -0700

    task zero

--- _printf.c
+++ _printf.c
@@ -9,27 +9,61 @@
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-
+	int printed = 0;
 	va_list args;
 
 	va_start(args, format);
-
-	while (*format != '\0')
+	while (*format)
 	{
-	  if (*format == '%')
+	   if (*format == '%')
 	  {
-	    format++;
-	    count = picker(format, args, count);
-	    format++;
-	  }
-	  else
-	  {
- 	    putchar(*format);
-	    count++;
-	    format++;
-	  }
+	     format++;
+	     if (*format == '\0')
+	        break;
+	     switch (*format)
+	   {
+		case 'c':
+			{
+				char ch = va_arg(args, int);
+				putchar(ch);
+				printed++;
+			}
+			break;
+		case 's':
+			{
+				char *str = va_arg(args, char*);
+				if (*str != '\0')
+				{
+					while (*str){
+					putchar(*str);
+					str++;
+					printed++;
+			        	}
+				}
+				else 
+				{
+				        char *null_str = "(null)";
+					while (*null_str){
+					putchar(*null_str);
+					null_str++;
+					printed++;
+					}
+				}
+			}
+			break;
+		case '%':
+			putchar('%');
+			printed++;
+			break;
 	}
-	va_end(args);
-	return (count);
+	}
+	else
+	{
+		putchar(*format);
+		printed++;
+	}
+	format++;
+	}
+va_end(args);
+return (printed);
 }
