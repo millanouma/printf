eece6fd3b4a4f37cdaef52c22b9821f0e0903f35
millanouma <millanouma@gmail.com>
2023-09-18 04:18:35 -0700

    task zero

--- _printf.c
+++ _printf.c
@@ -1,49 +1,54 @@
 #include "main.h"
 /**
- * _printf - function that produces output according to a format.
- * @format: format to use
- * 
+ * _printf - function that produces
+ *  output according to a format.
+ * @format: format to use.
  * Return: formated output.
 */
 
 int _printf(const char *format, ...)
 {
 	int count = 0;
-        va_list lists;
-        va_start(lists, format); 
+	va_list lists;
 
-    while (*format)
-    {
-        if (*format == '%')
-       	{
-            format++;  
-            if (*format == '\0')
-                break; 
-		}
-              else if (*format == 's')
-	      {
-                char *str = va_arg(lists, char *);
-                while (*str != '\0')
+	va_start(lists, format);
+
+
+	while (*format == '%')
+	{
+		if (*format == '%')
 		{
-                    _putchar(*str);		
-	    	    count ++;
-		    str++;
-		}	
-            }
-         	else if (*format == 'c')
-	    {
-                int ch = va_arg(lists, int);
-		_putchar(ch);
-                return (count +1); 
-            }
-        	else
-	     {
-               putchar(*format);  
-               count++;
-            }
-         format++;  
-    }
- va_end(lists);
- return count;
-}
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == 's')
+			{
+				char *str = va_arg(lists, char *);
 
+				if (*str != '\0')
+				{
+					while (*str)
+					{
+						_putchar(*str);
+						count++;
+						str++;
+					}
+				}
+			}
+			else if (*format == 'c')
+			{
+				int ch = va_arg(lists, int);
+
+				_putchar(ch);
+				return (count + 1);
+			}
+		}
+		else
+			{
+				_putchar(*format);
+				count++; }
+		format++;
+	}
+	va_end(lists);
+	return (count);
+}
